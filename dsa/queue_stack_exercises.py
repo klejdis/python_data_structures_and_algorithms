bb0182013a5d26fdd4359c545841543d97510665
klejdis jorgji <klejdis.jorgji@y42.com>
2024-05-26 02:10:10 +0200

    stack queue exercises

--- dsa/queue_stack_exercises.py
+++ dsa/queue_stack_exercises.py
@@ -127,3 +127,119 @@ class MinStackList:
 # Exercise 3
 # plate stack
 
+class PlateStack:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.stacks = []
+
+    def push(self, value):
+        if not self.stacks or len(self.stacks[-1]) == self.capacity:
+            self.stacks.append([])
+        self.stacks[-1].append(value)
+
+    def pop(self):
+        if not self.stacks:
+            raise Exception("Stack is empty")
+        value = self.stacks[-1].pop()
+        if not self.stacks[-1]:
+            self.stacks.pop()
+        return value
+
+    def pop_at(self, index):
+        if index >= len(self.stacks):
+            raise Exception("Invalid index")
+        value = self.stacks[index].pop()
+        if not self.stacks[index]:
+            self.stacks.pop(index)
+        return value
+
+    def __str__(self):
+        return str(self.stacks)
+
+
+# Exercise 4
+# implement a queue using two stacks
+
+class MyQueue:
+    def __init__(self):
+        self.stack_newest = []
+        self.stack_oldest = []
+
+    def size(self):
+        return len(self.stack_newest) + len(self.stack_oldest)
+
+    def add(self, value):
+        self.stack_newest.append(value)
+
+    def shift_stacks(self):
+        if not self.stack_oldest:
+            while self.stack_newest:
+                self.stack_oldest.append(self.stack_newest.pop())
+
+    def peek(self):
+        self.shift_stacks()
+        return self.stack_oldest[-1]
+
+    def remove(self):
+        self.shift_stacks()
+        return self.stack_oldest.pop()
+
+    def __str__(self):
+        return str(self.stack_newest) + str(self.stack_oldest)
+
+
+# Exercise 5
+# animal shelter for dogs and cats, only oldest animal can be adopted
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.order = 0
+
+    def __str__(self):
+        return self.name
+
+
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Cat(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class AnimalShelter:
+    def __init__(self):
+        self.dogs = []
+        self.cats = []
+        self.order = 0
+
+    def enqueue(self, animal):
+        animal.order = self.order
+        self.order += 1
+        if isinstance(animal, Dog):
+            self.dogs.append(animal)
+        elif isinstance(animal, Cat):
+            self.cats.append(animal)
+
+    def dequeue_any(self):
+        if not self.dogs:
+            return self.dequeue_cat()
+        if not self.cats:
+            return self.dequeue_dog()
+        if self.dogs[0].order < self.cats[0].order:
+            return self.dequeue_dog()
+        else:
+            return self.dequeue_cat()
+
+    def dequeue_dog(self):
+        if not self.dogs:
+            raise Exception("No dogs available")
+        return self.dogs.pop(0)
+
+    def dequeue_cat(self):
+        if not self.cats:
+            raise Exception("No cats available")
+        return self.cats.pop(0)
